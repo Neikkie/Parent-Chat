074ce2d70cd50c14538115eabb77e905c9269e37
Neikkie <neikkie21@outlook.com>
2026-04-06 20:50:56 -0400

    Initial Commit

---
+++ Parent Chat/Parent_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parent_ChatApp.swift
+//  Parent Chat
+//
+//  Created by Shanique Beckford on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Parent_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
